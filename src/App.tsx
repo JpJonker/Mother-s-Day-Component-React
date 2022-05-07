f67e3f4dbdfa09046694b10befca8bcdb22b2ac6
Dev JP <jpjonker.business@gmail.com>
2022-05-07 19:23:14 +0200

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
 
-function App() {
+import "./App.scss";
+
+const MotherDayCard = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 2, delay: 1 }}
+        className='app__card app__flex'
+      >
+        <motion.div
+          animate={{ x: [-1000, 0] }}
+          transition={{ duration: 2, delay: 2, ease: "backOut" }}
+          className='app__card-header'
         >
-          Learn React
-        </a>
-      </header>
+          <h1>Happy Mothers Day</h1>
+        </motion.div>
+        <motion.p
+          animate={{ x: [1000, 0] }}
+          transition={{ duration: 2, delay: 2.75, ease: "backOut" }}
+          className='app__card-text'
+        >
+          I want to wish you a very happy mothers day, I want to thank you for everything that you
+          have done for me in my life. I love you so much mum.
+        </motion.p>
+        <motion.div
+          animate={{ scale: [1, 3], opacity: [0, 1] }}
+          transition={{ duration: 1, delay: 4, ease: "easeInOut" }}
+          className='app__card-image'
+        >
+          <motion.div>
+            <motion.h2
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 1, delay: 5, ease: "backOut" }}
+            >
+              I
+            </motion.h2>
+            <motion.h2
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 1, delay: 7, ease: "backOut" }}
+            >
+              Mum
+            </motion.h2>
+            <motion.h2
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 1, delay: 6, ease: "backOut" }}
+            >
+              Y
+            </motion.h2>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div className='app__container app__flex'>
+      <MotherDayCard />
     </div>
   );
-}
+};
 
 export default App;
